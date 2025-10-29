0949ad8ab2595f53c05b7c4ffc13312341d3fe52
hetvianbizz-sudo <hetvianbizz@gmail.com>
2025-10-29 17:53:20 +0530

    fix the url issue

--- src/context/SocketContext.jsx
+++ src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("https://chembizz-app.vercel.app", {
+			const socket = io("https://chembizz-backend.onrender.com", {
 			// const socket = io("https://api.chembizz.in", {
 				query: {
 					userId: authUser.id,
